e92d9d1a61dbe68c7e80a24aabc042ca1fcaa274
anikmaahmud <106667426+anikmaahmud@users.noreply.github.com>
2022-06-25 19:07:17 +0600

    Added Lab Task 1

---
+++ Lab Task 1/LabTask1.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<head>
+    <title>home page</title>
+</head>
+<body>
+   <div>
+        <div>
+            <div>
+            <h2>DONOR INFORMATION</h2>
+                <form action="" method="post">
+                    <table>
+                        <tr>
+                            <td>
+                                First Name
+                            </td>
+                            <td>
+                                <input type="text" name="fname">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Last Name
+                            </td>
+                            <td>
+                                <input type="text" name="lname">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Company
+                            </td>
+                            <td>
+                                <input type="text" name="company">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Address 1
+                            </td>
+                            <td>
+                                <input type="text" name="ad1">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Address 2
+                            </td>
+                            <td>
+                                <input type="text" name="ad2">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                City
+                            </td>
+                            <td>
+                                <input type="text" name="city">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                State
+                            </td>
+                            <td>
+                                <input type="text" name="state">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Zipcode
+                            </td>
+                            <td>
+                                <input type="text" name="zipcode">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Country
+                            </td>
+                            <td>
+                                <input type="text" name="country">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Phone
+                            </td>
+                            <td>
+                                <input type="tel" name="phone">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Fax
+                            </td>
+                            <td>
+                                <input type="text" name="fax">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Email
+                            </td>
+                            <td>
+                                <input type="email" name="email">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Donation Ammount
+                            </td>
+                            <td>
+                                <input type="radio" name="donAmm">$10
+                                <input type="radio" name="donAmm">$20
+                                <input type="radio" name="donAmm">$50
+                                <input type="radio" name="donAmm">$100
+                                <input type="radio" name="donAmm">None
+                                <input type="radio" name="donAmm">Other
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Other Ammount
+                            </td>
+                            <td>
+                                <input type="text" name="othAmm">
+                            </td>
+                        </tr>
+                        <tr>
+                        <td>
+                        <span>Recurring Donation <input type="checkbox"> I am interessted in giving in a regular basis <br> Monthly Credit Card $ <input type="text"> <br> For <input type="text"> Months</span>
+                        </td>
+                        </tr>
+                    </table>
+                    <div>
+                        <input type="submit" value="Submit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
